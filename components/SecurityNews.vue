7185034fe6770884a2970d4414c81c447488a336
Charlotte Fereday <cbrfereday@gmail.com>
2021-04-05 21:33:05 +0100

    Gets the security news page to work

--- components/SecurityNews.vue
+++ components/SecurityNews.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <h1>Fetching Dev to </h1>
+    <h1>Fetching latest DevTo articles on security</h1>
     <ul>
       <li v-for="article in articles" :key="article.title">
+        <!-- todo: how use internal links well -->
         <!-- <NuxtLink :to="{ name: 'articles-slug', params: { slug: article.slug } }"> -->
-         {{ article.title }}
-          {{ article.description }}
-          {{ article.url }}
+        <h1>{{ article.title }}</h1>
+        <p>{{ article.description }}</p>
+        <a>{{ article.url }}</a>
         <!-- </NuxtLink> -->
       </li>
     </ul>
@@ -14,10 +15,22 @@
 </template>
 <script>
 export default {
-  async asyncData({ $http }) {
-    const articles = await $http.$get('https://dev.to/api/articles?per_page=3&tag=security')
-    console.log('articles', articles)
-    return { articles }
+  //:todo understand why doesn't this work?
+  // async asyncData({ $http }) {
+  //   const articles = await $http.$get('https://dev.to/api/articles?per_page=3&tag=security')
+  //   console.log('articles', articles)
+  //   return { articles }
+  // },
+
+  data() {
+    return {
+      articles: []
+    };
+  },
+  async fetch() {
+    this.articles = await fetch(
+      "https://dev.to/api/articles?per_page=3&tag=security"
+    ).then(res => res.json());
   }
-}
+};
 </script>
